ef61f8986a9ed53638cc2a4edf941e3c87c9a998
hirossan4049 <haruto405329@gmail.com>
2020-04-27 14:31:31 +0900

    feat: setting close mainApp Restart

--- app/app.py
+++ app/app.py
@@ -4,7 +4,6 @@ import kivy
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.factory import Factory
-
 from kivy.uix.boxlayout import BoxLayout
 
 from kivy.core.text import LabelBase, DEFAULT_FONT
@@ -22,25 +21,31 @@ from uix.main import MainWindow   # 追加
 from uix.settings import SettingsWindow
 
 
-
+isNoExit = False
 class MainRoot(BoxLayout):
     window1 = None
     window2 = None
 
     def __init__(self, **kwargs):
-# 起動時に各画面を作成して使い回す
+        # 起動時に各画面を作成して使い回す
         self.window1 = Factory.MainWindow()
         self.window2 = Factory.SettingsWindow()
         super(MainRoot, self).__init__(**kwargs)
         self.change_disp()    # 追加
 
     def change_disp(self):
+        global isNoExit
+        isNoExit = False
+        print("IS NO EXIT = FALSE")
         self.clear_widgets()
         self.add_widget(self.window1)
         Window.size = 500, 300
 
 
     def change_disp2(self):
+        global isNoExit
+        isNoExit = True
+        print("IS NO EXIT = TRUE")
         self.clear_widgets()
         self.add_widget(self.window2)
         Window.size = 1000,750
@@ -55,5 +60,19 @@ class MainApp(MDApp):
 
 
 
+def reset():
+    import kivy.core.window as window
+    from kivy.base import EventLoop
+    if not EventLoop.event_listeners:
+        from kivy.cache import Cache
+        window.Window = window.core_select_lib('window', window.window_impl, True)
+        Cache.print_usage()
+        for cat in Cache._categories:
+            Cache._objects[cat] = {}
+
 if __name__ == "__main__":
     MainApp().run()
+    while isNoExit:
+        print("IS NO EXIT")
+        reset()
+        MainApp().run()
--- app/uix/settings.py
+++ app/uix/settings.py
@@ -3,13 +3,15 @@ from kivymd.app import MDApp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 import configparser
+from pprint import pprint
 
-#FIXME:aaa
 
 
 class SSLSetting(Screen):
     def __init__(self,**kwargs):
         super(SSLSetting,self).__init__(**kwargs)
+        print("SSLSETTINGS!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
         config_file = configparser.ConfigParser()
         config_file.read('settings/config.ini')
 
@@ -18,16 +20,53 @@ class SSLSetting(Screen):
         self.certPath    = config_file.get("SSLSettings","CertPath")
         self.keyPath     = config_file.get("SSLSettings","KeyPath")
 
-        #config_file["SSLSettings"]["enable"] = "False"
-        #config_file.set("SSLSettings","enable","False")
-        #with open("settings/config.ini","w") as h:
-        #    config_file.write(h)
-
         self.ids.enableSwitch.active = self.ssl_enable
         self.ids.opensslPathTextField.text = self.openSSLPath
         self.ids.certPathTextField.text = self.certPath
         self.ids.keyPathTextField.text = self.keyPath
-        
+                 
+
+    def press_ok(self):
+        MDApp.get_running_app().stop()
+    def press_cancel(self):
+        pass
+    def press_apply(self):
+        print("PRESS APPLY")
+        self.settings_save()
+
+    def settings_save(self):
+        for settings,content,arg,isChanged in self.check_changeSettings():
+            if isChanged:
+                print("is changed!",content,arg)
+                self.save_config(settings,content,arg)
+                print("save done!")
+
+
+    # FIXME;汚ったねぇｗｗ
+    def check_changeSettings(self):
+        """設定に変更があるか確認"""
+        # SSL Settings
+        ssl = "SSLSettings"
+        arg = self.ids.enableSwitch.active
+        yield ssl, "enable", arg, not self.ssl_enable == arg
+
+        arg = self.ids.opensslPathTextField.text 
+        yield ssl, "OpenSSLPath", arg, not self.openSSLPath == arg
+
+        arg = self.ids.certPathTextField.text 
+        yield ssl, "CertPath", arg, not self.certPath == arg 
+
+        arg = self.ids.keyPathTextField.text 
+        yield ssl, "KeyPath", arg, not self.keyPath == arg
+
+
+    def save_config(self, settings, content, arg):
+        config = configparser.ConfigParser()
+        config.read("settings/config.ini")
+        config.set(settings,content,str(arg))
+        with open("settings/config.ini","w") as h:
+            config.write(h)
+
 class LangSetting(Screen):
     pass
 class DarkmodeSetting(Screen):
@@ -39,6 +78,7 @@ class DeveloperSetting(Screen):
 class SettingsWindow(BoxLayout):
     def __init__(self,**kwargs):
         super(SettingsWindow,self).__init__(**kwargs)
+       
         self.ids.setting_screen_manager.add_widget(SSLSetting(name="ssl"))
         self.ids.setting_screen_manager.add_widget(LangSetting(name="lang"))
         self.ids.setting_screen_manager.add_widget(DarkmodeSetting(name="darkmode"))
@@ -46,20 +86,54 @@ class SettingsWindow(BoxLayout):
         self.ids.setting_screen_manager.transition = NoTransition()
         #self.ids.setting_screen_manager.switch_to(SSLSetting())
 
-
-
     def gotoSSL(self):
-        #self.ids.setting_screen_manager.switch_to(LangSetting())
+        #self.ids.setting_screen_manager.switch_to(SSLSetting())
         self.ids.setting_screen_manager.current = "ssl"
-
     def gotoLang(self):
+        #self.ids.setting_screen_manager.switch_to(LangSetting())
         self.ids.setting_screen_manager.current = "lang"
-
     def gotoDarkmode(self):
         self.ids.setting_screen_manager.current = "darkmode"
-
     def gotoDeveloper(self):
         self.ids.setting_screen_manager.current = "developer"
+    
+    def press_ok(self):
+        pass
+    def press_cancel(self):
+        pass
+    def press_apply(self):
+        self.settings_save()
+
+    def settings_save(self):
+        for settings,content,arg,isChanged in self.check_changeSettings():
+            if isChanged:
+                print("is changed!",content,arg)
+
+
+    # FIXME;汚ったねぇｗｗ
+    def check_changeSettings(self):
+        """設定に変更があるか確認"""
+        # SSL Settings
+        ssl = "SSLSettings"
+        arg = self.ids.enableSwitch.active
+        yield ssl, "enable", arg, self.ssl_enable == arg
+
+        arg = self.ids.opensslPathTextField.text 
+        yield ssl, "OpenSSLPath", arg, self.openSSLPath == arg
+
+        arg = self.ids.certPathTextField.text 
+        yield ssl, "CertPath", arg, self.certPath == arg 
+
+        arg = self.ids.keyPathTextFielf.text 
+        yield ssl, "KeyPath", arg, self.keyPath == arg
+        # Lang Settings
+
+    def save_config(self, settings, content, arg):
+        config = configparser.ConfigParser()
+        config.read("settings/config.ini")
+        config.set(settings,content,arg)
+        with open("settings/config.ini") as h:
+            config.write(h)
 
 
 
