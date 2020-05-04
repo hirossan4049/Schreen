0ea9567c5596bfbce3d9117e5f41575aaff300f3
hirossan4049 <haruto405329@gmail.com>
2020-05-04 17:07:29 +0900

    add filebrowser folder and file (ry

--- app/uix/filemanager.py
+++ app/uix/filemanager.py
@@ -5,6 +5,10 @@ from kivy.uix.modalview import ModalView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import StringProperty
+from kivy.uix.behaviors import ButtonBehavior  
+from kivy.graphics import *
+import time
+import os
 
 # FIXME:なんでデフォルトの（ｒｙhh
 # [x] Kivy Filemanager -> ダサい
@@ -12,6 +16,7 @@ from kivy.properties import StringProperty
 # [x] tkinter filemanager -> なんか怒られた
 
 # RecycleView使ってないけど許せ
+# Windows not friendly :)
 
 # TODO: [ ] PACKAGE CODING
 
@@ -26,14 +31,21 @@ Builder.load_string("""
     size_hint:None,None
     BoxLayout:
         orientation:"vertical"
-        MDLabel:
+        BoxLayout:
             size_hint:1,None
-            padding:30,10
-#            pos_hint: {"center_y": 0.9}
-            text:"hello"
-            font_size:45
-            color:0.32, 0.43, 0.47,1
-            height: self.texture_size[1]
+            MDIconButton:
+                icon:"undo"
+                on_release:root.undo()
+                size_hint:None,None
+            MDLabel:
+                id:path_name
+                size_hint:1,None
+                padding:30,10
+    #            pos_hint: {"center_y": 0.9}
+                text:"hello"
+                font_size:45
+                color:0.32, 0.43, 0.47,1
+                height: self.texture_size[1]
 
         MDSeparator:
 
@@ -57,12 +69,12 @@ Builder.load_string("""
             on_release:root.dismiss()      
 <IconListItem>:
     size_hint:None,None
-    size:300,300
+    size:250,250
     orientation:"vertical"
     Image:
         #text:root.source
-        source:"images/folder.png"
-        size_hint:1,None
+        source:root.source
+        size_hint:1,1
     MDLabel:
         text_size:self.size
         halign:"center"
@@ -70,21 +82,104 @@ Builder.load_string("""
         size_hint:1,None
 """ )
 
-class IconListItem(BoxLayout):
+DIRIMAGE = "images/folder.png"
+IMAGEIMAGE = "images/imageImage.png"
+
+# Ontouchdown doble click? No.
+class IconListItem(ButtonBehavior,BoxLayout):
     source = StringProperty()
     filename = StringProperty()
-    def __init__(self,**kwargs):
+    now_dir = StringProperty()
+
+    def __init__(self,on_press,**kwargs):
         super().__init__(**kwargs)
+        #print(kwargs)
+        self.filepath = kwargs["now_dir"] + "/" + kwargs["filename"]
+        self.on_press = on_press
+        self.isdir = False
+        self.filecheck()
+
+    
+    def filecheck(self):
+        if os.path.isdir(self.filepath):
+            self.isdir = True
+            self.source = "images/folder.png"
+        elif os.path.isfile(self.filepath):
+            self.source = "images/imageImage.png"
+    def imget(self):
+        pass
+    def setIcon(self,imagepath):
+        pass
+    def on_release(self,*args):
+        self.on_press(self.filename,self.isdir,self.filepath)
+        #with self.canvas:
+        #    Color(.1, .6, .3,.3)
+        #    Rectangle(pos=self.pos, size=self.size)
+
+
+
 
 class BeautifulFileManager(ModalView):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
+        self.baclick = ""   
+        #self.backfilepath = ""
+        self.backtime = 0
+        self.undo_path = []
+        self.add_stack()
+
+        #homedir = os.path.expanduser("~")
+        #files = os.listdir(homedir)
        
-        for i in range(10):
-            self.ids.stackLayout.add_widget(IconListItem(source="hello",filename="filename"+str(i)))
+        #for fileitem in files:
+        #    self.ids.stackLayout.add_widget(IconListItem(
+        #                                        on_press=self.filepressed,
+        #                                        #source="hello",
+        #                                        filename=fileitem,
+        #                                        now_dir=homedir))
+
+    def filepressed(self,*args):
+        if not args:
+            return
+        filepath,isdir,filepath = args
+        if self.baclick == filepath:
+            if time.time() - self.backtime <= 1:
+                print("dble Clicked")
+                if isdir:
+                    self.add_stack(dir=filepath)
+        else:
+            self.baclick = filepath
+            self.backtime = time.time()
+
+    def stack_clear(self):
+        self.ids.stackLayout.clear_widgets()
+            
+    def add_stack(self,dir="~"):
+        self.stack_clear()
+        homedir = os.path.expanduser(dir)
+        self.undo_path.append(homedir)
+        self.ids.path_name.text = homedir
+        files = os.listdir(homedir)
+        for fileitem in files:
+            self.ids.stackLayout.add_widget(IconListItem(
+                                                on_press=self.filepressed,
+                                                #source="hello",
+                                                filename=fileitem,
+                                                now_dir=homedir))
         
+    def undo(self):
+        undo_path = ""
+        try:
+            undo_path = self.undo_path.pop(-2)
+            del self.undo_path[-1]
+        except IndexError:
+            from kivymd.toast import toast
+            toast("これ以上戻れません")
+            return
+        self.add_stack(undo_path)
+
         
-    pass
+    
 
 class TestWindow(FloatLayout):
     def __init__(self,**kwargs):
